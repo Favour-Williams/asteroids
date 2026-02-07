9f1181a05cc2eee13886b22fc8162df500c3d270
Favour-Williams <williamsfavour012345@gmail.com>
2026-02-07 13:39:29 +0200

    added shape

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0 # delta time(means change), time between frames
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,15 +17,21 @@ def main():
         log_state()
 
         screen.fill("black")
+
+        player.draw(screen)
+        
         pygame.display.flip()
         milliseconds = clock.tick(60) # 60 frames per second
 
         dt = milliseconds / 1000
 
+    
+    
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
     
 
